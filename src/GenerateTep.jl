c41b8312417c513e2ee26939fe9d2741b4ed8906
Tim Van Wesemael <tim.vanwesemael@ugent.be>
2024-10-19 10:37:21 -0300

    Modularized functionality

---
+++ src/GenerateTep.jl
@@ -0,0 +1,16 @@
+module GenerateTep
+
+using ArgParse
+using Graphs
+using NetworkJumpProcesses
+using JumpProcesses
+
+export generate_jump_sets
+export solve_problem
+export parse_command_line_args
+export to_tep
+
+include(joinpath(@__DIR__, "gillespie_sis.jl"))
+include(joinpath(@__DIR__, "preparation.jl"))
+
+end
---
+++ src/preparation.jl
@@ -0,0 +1,77 @@
+function parse_command_line_args()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--n"
+        help = "Number of vertices"
+        arg_type = Int64
+        default = 1000
+
+        "--p"
+        help = "Edge probability"
+        arg_type = Float64
+        default = 0.01
+
+        "--N"
+        help = "Number of teps for the same graph"
+        arg_type = Int64
+        default = 1
+
+        "--lambda"
+        help = "Infection rate"
+        arg_type = Float64
+        default = 0.03
+
+        "--mu"
+        help = "Healing rate"
+        arg_type = Float64
+        default = 0.09
+
+        "--T"
+        help = "Time period"
+        arg_type = Float64
+        default = 100.0
+
+        "--dt"
+        help = "Sampling step; if nothing is given, the exact tep is returned"
+        arg_type = Float64
+        default = nothing
+    end
+
+    return parse_args(s)
+end
+
+"""
+    to_tep(sol)
+
+Convert the solution `sol` to an exact tep given by a two row matrix, where the first row
+contains the time points and the second row contains the vertex indices.
+"""
+function to_tep(sol::ODESolution)
+    t = Vector{Float64}(undef, length(sol.t))
+    x = Vector{Int64}(undef, length(sol.t))
+    t[1] = 0.
+    x[1] = findfirst(sol.u[1] .== 1)[1]
+    c_idx = 1
+    for i in 2:length(t)
+        options = findall(sol.u[i] .!= sol.u[i-1])
+        if length(options) > 0
+            c_idx += 1
+            t[c_idx] = sol.t[i]
+            x[c_idx] = options[1]
+        elseif length(options) > 1
+            @error "Multiple changes at the same time"
+        end
+    end
+    return [t[1:c_idx] Float64.(x[1:c_idx])]
+end
+
+"""
+    to_tep(sol, dt)
+
+Convert the solution `sol` to a sampled tep in the form of a matrix.
+Each column corresponds to a time point and each row corresponds to a vertex.
+"""
+function to_tep(sol::ODESolution, dt::Real)
+    return hcat([sol(t) for t in 0:dt:sol.t[end]]...)
+end
